669730f82bab710423e01f789fc57cdfb041c6b9
Arshad3620 <ridoy3620@gmail.com>
2025-07-13 15:26:36 +0900

    Initial commit on main branch

---
+++ aws python/Upload_file_s3.py
@@ -0,0 +1,6 @@
+# import boto3
+
+# s3 = boto3.client('s3')
+# s3.upload_file('local_file.txt', 'your-bucket-name', 'remote_file.txt')
+# # Replace 'local_file.txt' with the path to your local file
+# # Replace 'your-bucket-name' with the name of your S3 bucket
---
+++ aws python/main.py
@@ -0,0 +1,15 @@
+import random
+
+total_guesses = 2
+
+popular_dict = {
+  'usa = 22'
+  'canada = 15',
+  'mexico = 10',
+  'brazil = 8',
+  'argentina = 5',
+  'france = 20',
+}
+def main ():
+  print("welcome to the guesing game ")
+  
